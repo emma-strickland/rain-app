e938fad3d158deaccfd5187a4dced8edd8936369
Emma Strickland <emlstrick@gmail.com>
2021-06-28 14:28:29 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
+const api = {
+  key: "93da148cde43b30ed7c6df2296294e89",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
 function App() {
+
+  // const dateBuilder = (date) => {
+  //   let date = new Date().toDateString()
+  // }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Enter zip code"
+          />
+        </div>
+        <div className="location-box">
+          <div className="location">Brooklyn, NY, USA</div>
+          <div classname="date">{new Date().toDateString()}</div>
+        </div>
+        <div className="rain">
+          <div className="rain-true">Yes, it will rain today</div>
+          <div className="rain-false">No, it will not rain today :(</div>
+        </div>
+      </main>
     </div>
   );
 }
