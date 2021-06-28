37cdb72d4fbdb64046d77918b721a086d61c3955
Emma Strickland <emlstrick@gmail.com>
2021-06-28 18:28:14 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import React, { useState } from 'react';
+
 
 const api = {
   key: "93da148cde43b30ed7c6df2296294e89",
@@ -6,10 +8,21 @@ const api = {
 }
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [rain, setRain] = useState({});
 
-  // const dateBuilder = (date) => {
-  //   let date = new Date().toDateString()
-  // }
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=imperial&appid=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setRain(result);
+          setQuery('');
+          console.log(result)
+        });
+    };
+  }
+
   return (
     <div className="App">
       <main>
@@ -18,16 +31,25 @@ function App() {
             type="text"
             className="search-bar"
             placeholder="Enter zip code"
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
           />
         </div>
-        <div className="location-box">
-          <div className="location">Brooklyn, NY, USA</div>
-          <div classname="date">{new Date().toDateString()}</div>
-        </div>
-        <div className="rain">
-          <div className="rain-true">Yes, it will rain today</div>
-          <div className="rain-false">No, it will not rain today :(</div>
-        </div>
+        {(typeof rain.main != "undefined") ? (
+          <div>
+            <div className="location-box">
+              <div className="location">{rain.name}, {rain.sys.country}</div>
+              <div className="date">{new Date().toDateString()}</div>
+            </div>
+            <div className="rains">
+              <div className="rain-true">{(rain.main === "rain")}
+
+                Yes, it will rain today!</div>
+              <div className="rain-false">No, it will not rain today :(</div>
+            </div>
+          </div>
+        ) : ('')}
       </main>
     </div>
   );
